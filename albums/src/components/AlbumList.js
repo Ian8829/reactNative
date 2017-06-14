4ad7f32f8d7a12516b5f71c7605e453d813fef09
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-14 16:53:22 +0900

    Lifecycle Methods

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 export default class AlbumList extends Component {
+  componentWillMount() {
+    console.log('componentWillMount in AlbumList');
+  }
+
   render() {
     return(
       <View>
