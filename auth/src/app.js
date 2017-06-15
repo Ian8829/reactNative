f25deef3ac9daa31a6cb1a142055f7e5e746041c
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-15 14:40:55 +0900

    Copying Reusable Components

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Header, Button, Card, CardSection } from './components/common';
 
 export default class App extends Component {
   render() {
     return(
       <View>
+        <Header headerText="Authentication"/>
         <Text>An app!</Text>
       </View>
     );
---
+++ auth/src/components/common/index.js
@@ -0,0 +1,8 @@
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+// If export like upside,
+// You should export children as
+// export { Button };
+
