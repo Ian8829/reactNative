453645857bd257e7eec0a60a94791b255d9aedd9
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-15 12:13:07 +0900

    Displaying Album Artwork

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -4,13 +4,19 @@ import Card from './Card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, headerContentStyle } = styles;
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
 
   return(
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }}
@@ -18,10 +24,16 @@ const AlbumDetail = ({ album }) => {
         </View>
         <View style={headerContentStyle}>
           {/*styles.headerContentStyle*/}
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
           {/*props.album.artist*/}
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          source={{ uri: image }}
+          style={imageStyle}
+        />
       </CardSection>
     </Card>
 
@@ -33,9 +45,23 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
