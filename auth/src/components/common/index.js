253168c6b1d1ec4db67b5206e9d2293b05da137d
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-15 15:44:45 +0900

    Making Text Inputs From Scratch

--- auth/src/components/common/index.js
+++ auth/src/components/common/index.js
@@ -2,6 +2,7 @@ export * from './Button';
 export * from './Card';
 export * from './CardSection';
 export * from './Header';
+export * from './Input';
 // If export like upside,
 // You should export children as
 // export { Button };
