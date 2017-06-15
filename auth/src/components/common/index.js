841f6cc52e670b33a8d85b82cd73415041beae1e
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-15 17:15:40 +0900

    More Authentication, Spinner

--- auth/src/components/common/index.js
+++ auth/src/components/common/index.js
@@ -3,6 +3,7 @@ export * from './Card';
 export * from './CardSection';
 export * from './Header';
 export * from './Input';
+export * from './Spinner';
 // If export like upside,
 // You should export children as
 // export { Button };
