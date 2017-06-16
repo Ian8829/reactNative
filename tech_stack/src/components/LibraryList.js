579c2a9ea22f3732c1891c66373a5b1aee540491
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-16 14:10:40 +0900

    Connect Function

---
+++ tech_stack/src/components/LibraryList.js
@@ -0,0 +1,12 @@
+import React, { Components } from 'react';
+import { connect } from 'react-redux';
+
+class LibraryList extends Components {
+  render() {
+    return(
+
+    );
+  }
+}
+
+export default connect()(LibraryList);
