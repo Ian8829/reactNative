d748c83a6bd96e99cf4b4f57c8958376d85dd185
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-16 13:37:49 +0900

    Rendering the Header

---
+++ tech_stack/src/components/common/index.js
@@ -0,0 +1,10 @@
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+export * from './Input';
+export * from './Spinner';
+// If export like upside,
+// You should export children as
+// export { Button };
+
