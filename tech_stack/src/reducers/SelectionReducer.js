3d2a76e9915c9648554eb380338e6c9e1b7975b3
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-16 15:58:46 +0900

    Rules of Reducers

--- tech_stack/src/reducers/SelectionReducer.js
+++ tech_stack/src/reducers/SelectionReducer.js
@@ -1,5 +1,10 @@
-export default (state, action) => {
-  console.log(action);
-  return null;
-  // we do not currently have selected.
+export default (state = null, action) => {
+  // if this argument is undefined,
+  // instead return value as null
+  switch (action.type) {
+    case 'select_library':
+      return action.payload;
+    default:
+      return state;
+  }
 };
