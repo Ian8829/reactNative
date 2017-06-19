a107f10561409c2cf2cdbfe6579a53185b49d746
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-19 17:14:05 +0900

    Handling Form Updates at the Reducer Level

--- manager/src/reducers/EmployeeFormReducer.js
+++ manager/src/reducers/EmployeeFormReducer.js
@@ -2,10 +2,24 @@ import {
   EMPLOYEE_UPDATE
 } from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+  name: '',
+  phone: '',
+  shift: ''
+};
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case EMPLOYEE_UPDATE:
+      // action.payload === { prop: 'name', value: 'jane' }
+      return {
+        ...state,
+        [action.payload.prop]: action.payload.value
+      };
+     // key interpolation
+     // [action.payload.prop] -> name || phone || shift..
+     // const newState = {};
+     // newState[action.payload.prop] = action.payload.value
     default:
       return state;
   }
