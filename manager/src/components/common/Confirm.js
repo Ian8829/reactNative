8ec4e6bdcef1f8277b5861ed33e42490cee93429
taeshik Hwang <ian@taeshiks-MacBook-Pro.local>
2017-06-20 16:28:23 +0900

    Final

--- manager/src/components/common/Confirm.js
+++ manager/src/components/common/Confirm.js
@@ -40,7 +40,7 @@ const styles = {
     lineHeight: 40
   },
   containerStyle: {
-    backgroundColor: 'rgba(0, 0, 0, 0.75)',
+    backgroundColor: '#fff3ee',
     position: 'relative',
     flex: 1,
     justifyContent: 'center'
